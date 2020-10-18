717c4ceb270323c2370f35aa90ee04bfd2363986
Ivan <gefestroyale@gmail.com>
2020-10-18 15:02:22 +0300

    [0.00.5]: Assertions: use StraitX codestyle

--- Engine/sources/core/assert.cpp
+++ Engine/sources/core/assert.cpp
@@ -3,7 +3,7 @@
 #include <cstdio>
 #include <cstdlib>
 
-void assert_fail(const char *assertion, const char *filename, unsigned int line, const char * function){
+void AssertFail(const char *assertion, const char *filename, unsigned int line, const char * function){
     printf("[ERROR]: Assertion %s failed\nfile: %s\nline: %u\nfunction: %s\n",assertion,filename,line,function);
     exit(EXIT_FAILURE);
 }
--- Engine/sources/core/assert.hpp
+++ Engine/sources/core/assert.hpp
@@ -3,9 +3,10 @@
 
 #include "platform/compiler.hpp"
 
-extern void assert_fail(const char *assertion, const char *filename, unsigned int line, const char * function);
+extern void AssertFail(const char *assertion, const char *filename, unsigned int line, const char * function);
 
 
-#define assert(expression) (static_cast<bool>(expression)? (void)0 : assert_fail(#expression,SX_THIS_FILE,SX_THIS_LINE,SX_THIS_FUNCTION))
+#define Assert(expression) (static_cast<bool>(expression)? (void)0 : AssertFail(#expression,SX_THIS_FILE,SX_THIS_LINE,SX_THIS_FUNCTION))
+
 
 #endif // STRAITX_ASSERT_HPP
