36e874e0d516391a677e2c3879d9707220606cec
Ivan <gefestroyale@gmail.com>
2020-10-19 11:34:15 +0300

    [0.00.5]: undefine assertions in release builds

--- Engine/sources/core/assert.hpp
+++ Engine/sources/core/assert.hpp
@@ -5,8 +5,10 @@
 
 extern void AssertFail(const char *assertion, const char *filename, unsigned int line, const char * function);
 
-
-#define Assert(expression) (static_cast<bool>(expression)? (void)0 : AssertFail(#expression,SX_THIS_FILE,SX_THIS_LINE,SX_THIS_FUNCTION))
-
+#ifdef SX_DEBUG
+    #define Assert(expression) (static_cast<bool>(expression)? (void)0 : AssertFail(#expression,SX_THIS_FILE,SX_THIS_LINE,SX_THIS_FUNCTION))
+#else
+    #define Assert(expression)
+#endif
 
 #endif // STRAITX_ASSERT_HPP
