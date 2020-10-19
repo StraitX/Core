95a6d008dc3b6b8d3eca20c38d04cffa464ad55f
Ivan <gefestroyale@gmail.com>
2020-10-19 16:28:21 +0300

    [0.00.7]: Math: Dot product

---
+++ Engine/sources/core/math/linear.hpp
@@ -0,0 +1,30 @@
+#ifndef STRAITX_LINEAR_HPP
+#define STRAITX_LINEAR_HPP
+
+#include "core/math/vector2.hpp"
+#include "core/math/vector3.hpp"
+#include "core/math/vector4.hpp"
+
+namespace StraitX{
+
+
+template <typename T>
+T dot(const Vector2<T> &left, const Vector2<T> &right){
+    return left.x * right.x + left.y * right.y;
+}
+
+template <typename T>
+T dot(const Vector3<T> &left, const Vector3<T> &right){
+    return left.x * right.x + left.y * right.y + left.z * right.z;
+}
+
+template <typename T>
+T dot(const Vector4<T> &left, const Vector4<T> &right){
+    return left.x * right.x + left.y * right.y + left.z * right.z + left.w * right.w;
+}
+
+
+}; // namespace StraitX::
+
+
+#endif // STRAITX_LINEAR_HPP
