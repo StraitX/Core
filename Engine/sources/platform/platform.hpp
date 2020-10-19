8e87961206c4d1f173144e8931795fd375abf70c
Ivan <gefestroyale@gmail.com>
2020-10-19 20:11:25 +0300

    [0.00.7]: Add platfrom name define

--- Engine/sources/platform/platform.hpp
+++ Engine/sources/platform/platform.hpp
@@ -4,10 +4,12 @@
 #if defined(_WIN32)
 
     #define SX_PLATFORM_WINDOWS
+    #define SX_PLATFORM_NAME "Windows"
 
 #elif defined(__APPLE__) && defined(__MACH__)
 
     #define SX_PLATFORM_MACOS
+    #define SX_PLATFORM_NAME "MacOS"
     #error Apple operating systems are not supported yet
 
 #elif defined(__unix__)
@@ -15,20 +17,24 @@
     #if defined(__ANDROID__)
 
         #define SX_PLATFORM_ANDROID
+        #define SX_PLATFORM_NAME "Android"
         #error Android is not supported yet
 
     #elif defined(__linux__)
 
         #define SX_PLATFORM_LINUX
+        #define SX_PLATFORM_NAME "Linux"
 
     #elif defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
 
         #define SX_PLATFORM_FREEBSD
+        #define SX_PLATFORM_NAME "FreeBSD"
         #error FreeBSD is not supported yet
 
     #elif defined(__OpenBSD__)
 
         #define SX_PLATFORM_OPENBSD
+        #define SX_PLATFORM_NAME "OpenBSD"
         #error OpenBSD is not supported yet
 
     #else
