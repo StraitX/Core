31f8554d04ec834bae370cd37c816b03fe127dba
Ivan Hephaestus <gefestroyale@gmail.com>
2020-11-04 06:42:21 -0300

    [0.00.20]: Windows: Virtual Keys To StraitX conversions

--- Engine/sources/platform/windows/virtual_keys.cpp
+++ Engine/sources/platform/windows/virtual_keys.cpp
@@ -17,6 +17,19 @@ short MouseButtonToVirtualKey(Mouse::Button button) {
 	}
 }
 
+Mouse::Button VirtualKeyToMouseButton(short button) {
+	switch (button)
+	{
+    case 0:             return StraitX::Mouse::Unknown;
+    case VK_LBUTTON:    return StraitX::Mouse::Left;
+    case VK_RBUTTON:    return StraitX::Mouse::Right;
+    case VK_MBUTTON:    return StraitX::Mouse::Middle;
+    case VK_XBUTTON1:   return StraitX::Mouse::XButton1;
+    case VK_XBUTTON2:   return StraitX::Mouse::XButton2;
+    default:            return StraitX::Mouse::Unknown;
+	}
+}
+
 short KeyCodeToVirtualKey(Keyboard::KeyCode code) {
     if (code >= 65 && code <= 90 || code >= 48 && code <= 57)
         return (short)code;
@@ -106,6 +119,95 @@ short KeyCodeToVirtualKey(Keyboard::KeyCode code) {
     case StraitX::Keyboard::Menu: return VK_APPS;
     default: return 0;
 	}
+}
+
+Keyboard::KeyCode VirtualKeyToKeyCode(short code) {
+    if (code >= 65 && code <= 90 || code >= 48 && code <= 57)
+        return (Keyboard::KeyCode)code;
+    switch (code)
+    {
+    case 0: return StraitX::Keyboard::Unknown;
+    case VK_OEM_4: return StraitX::Keyboard::LeftBracket;
+    case VK_OEM_5: return StraitX::Keyboard::Backslash;
+    case VK_OEM_6: return StraitX::Keyboard::RightBracket;
+    case VK_SPACE: return StraitX::Keyboard::Space;
+    case VK_OEM_7: return StraitX::Keyboard::Quote;
+    case VK_OEM_COMMA: return StraitX::Keyboard::Comma;
+    case VK_OEM_MINUS:  return StraitX::Keyboard::Minus;
+    case VK_OEM_PERIOD:  return StraitX::Keyboard::Period;
+    case VK_OEM_2:  return StraitX::Keyboard::Slash;
+    case VK_OEM_1:  return StraitX::Keyboard::Semicolon;
+    case VK_OEM_PLUS: return StraitX::Keyboard::Equal;
+    case VK_OEM_3:  return StraitX::Keyboard::GraveAccent;
+    case VK_ESCAPE: return StraitX::Keyboard::Escape;
+    case VK_RETURN: return StraitX::Keyboard::Enter;
+    case VK_TAB: return StraitX::Keyboard::Tab;
+    case VK_BACK: return StraitX::Keyboard::Backspace;
+    case VK_INSERT: return StraitX::Keyboard::Insert;
+    case VK_DELETE: return StraitX::Keyboard::Delete;
+    case VK_RIGHT: return StraitX::Keyboard::Right;
+    case VK_LEFT: return StraitX::Keyboard::Left;
+    case VK_DOWN: return StraitX::Keyboard::Down;
+    case VK_UP: return StraitX::Keyboard::Up;
+    case VK_PRIOR: return StraitX::Keyboard::PageUp;
+    case VK_NEXT: return StraitX::Keyboard::PageDown;
+    case VK_HOME: return StraitX::Keyboard::Home;
+    case VK_END: return StraitX::Keyboard::End;
+    case VK_CAPITAL: return StraitX::Keyboard::CapsLock;
+    case VK_SCROLL: return StraitX::Keyboard::ScrollLock;
+    case VK_NUMLOCK: return StraitX::Keyboard::NumLock;
+    case VK_PRINT: return StraitX::Keyboard::PrintScreen;
+    case VK_PAUSE: return StraitX::Keyboard::Pause;
+    case VK_F1: return StraitX::Keyboard::F1;
+    case VK_F2: return StraitX::Keyboard::F2;
+    case VK_F3: return StraitX::Keyboard::F3;
+    case VK_F4: return StraitX::Keyboard::F4;
+    case VK_F5: return StraitX::Keyboard::F5;
+    case VK_F6: return StraitX::Keyboard::F6;
+    case VK_F7: return StraitX::Keyboard::F7;
+    case VK_F8: return StraitX::Keyboard::F8;
+    case VK_F9: return StraitX::Keyboard::F9;
+    case VK_F10: return StraitX::Keyboard::F10;
+    case VK_F11: return StraitX::Keyboard::F11;
+    case VK_F12: return StraitX::Keyboard::F12;
+    case VK_F13: return StraitX::Keyboard::F13;
+    case VK_F14: return StraitX::Keyboard::F14;
+    case VK_F15: return StraitX::Keyboard::F15;
+    case VK_F16: return StraitX::Keyboard::F16;
+    case VK_F17: return StraitX::Keyboard::F17;
+    case VK_F18: return StraitX::Keyboard::F18;
+    case VK_F19: return StraitX::Keyboard::F19;
+    case VK_F20: return StraitX::Keyboard::F20;
+    case VK_F21: return StraitX::Keyboard::F21;
+    case VK_F22: return StraitX::Keyboard::F22;
+    case VK_F23: return StraitX::Keyboard::F23;
+    case VK_F24: return StraitX::Keyboard::F24;
+    case VK_NUMPAD0: return StraitX::Keyboard::Keypad_0;
+    case VK_NUMPAD1: return StraitX::Keyboard::Keypad_1;
+    case VK_NUMPAD2: return StraitX::Keyboard::Keypad_2;
+    case VK_NUMPAD3: return StraitX::Keyboard::Keypad_3;
+    case VK_NUMPAD4: return StraitX::Keyboard::Keypad_4;
+    case VK_NUMPAD5: return StraitX::Keyboard::Keypad_5;
+    case VK_NUMPAD6: return StraitX::Keyboard::Keypad_6;
+    case VK_NUMPAD7: return StraitX::Keyboard::Keypad_7;
+    case VK_NUMPAD8: return StraitX::Keyboard::Keypad_8;
+    case VK_NUMPAD9: return StraitX::Keyboard::Keypad_9;
+    case VK_DECIMAL: return StraitX::Keyboard::KeypadDecimal;
+    case VK_DIVIDE: return StraitX::Keyboard::KeypadDivide;
+    case VK_MULTIPLY: return StraitX::Keyboard::KeypadMultiply;
+    case VK_SUBTRACT: return StraitX::Keyboard::KeypadSubstract;
+    case VK_ADD: return StraitX::Keyboard::KeypadAdd;
+    case VK_LSHIFT: return StraitX::Keyboard::LeftShift;
+    case VK_LCONTROL: return StraitX::Keyboard::LeftControl;
+    case VK_LMENU: return StraitX::Keyboard::LeftAlt;
+    case VK_LWIN: return StraitX::Keyboard::LeftSuper;
+    case VK_RSHIFT: return StraitX::Keyboard::RightShift;
+    case VK_RCONTROL: return StraitX::Keyboard::RightControl;
+    case VK_LMENU: return StraitX::Keyboard::RightAlt;
+    case VK_RWIN: return StraitX::Keyboard::RightSuper;
+    case VK_APPS: return StraitX::Keyboard::Menu;
+    default: return StraitX::Keyboard::Unknown;
+    }
 }
 
 }; // namespace Windows::
