25a0159dbcb2eea655e58d1a4be95d35c7816464
Ivan Hephaestus <gefestroyale@gmail.com>
2020-11-06 12:50:07 -0300

    [0.00.20]: Window: remove window implementation

--- Engine/sources/platform/windows/platform.cpp
+++ Engine/sources/platform/windows/platform.cpp
@@ -1,12 +1,10 @@
 #include "platform/platform.hpp"
 #include "platform/windows/hinstance.hpp"
-#include "platform/windows/window_win32.hpp"
 
 namespace StraitX {
 
 Error Platform::Initialize() {
     Windows::HInstance::Instance().Initialize();
-    Windows::WindowWin32::RegClass();
     return ErrorCode::Success;
 }
 
