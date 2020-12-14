b20e8e67fd20530206f717ec4abe3b6bb9ed3917
E1Hephaestus <gefestroyale@gmail.com>
2020-12-14 23:17:41 +0200

    [0.01.02]: Even more flexible Log macro

--- Engine/sources/core/log.hpp
+++ Engine/sources/core/log.hpp
@@ -9,28 +9,28 @@
 #ifdef SX_DEBUG
 
 #define DLog(source,error) StraitX::Output::Printf("[%]: %: %\n",error==Error::Success? "Info " : "Error",source, ErrorNames[(int)error])
-#define DLogTrace(msg) StraitX::Output::Printf("[Trace]: %\n", msg)
-#define DLogInfo(msg)  StraitX::Output::Printf("[Info ]: %\n", msg)
-#define DLogWarn(msg)  StraitX::Output::Printf("[Warn ]: %\n", msg)
-#define DLogError(msg) StraitX::Output::Printf("[Error]: %\n", msg)
+#define DLogTrace(...) {StraitX::Output::Printf("[Trace]: "); StraitX::Output::Printf(__VA_ARGS__); StraitX::Output::Printf("\n");}
+#define DLogInfo(...)  {StraitX::Output::Printf("[Info ]: "); StraitX::Output::Printf(__VA_ARGS__); StraitX::Output::Printf("\n");}
+#define DLogWarn(...)  {StraitX::Output::Printf("[Warn ]: "); StraitX::Output::Printf(__VA_ARGS__); StraitX::Output::Printf("\n");}
+#define DLogError(...) {StraitX::Output::Printf("[Error]: "); StraitX::Output::Printf(__VA_ARGS__); StraitX::Output::Printf("\n");}
 #define DLogSeparator() StraitX::Output::Print("===============================================================")
 
 #else
 
-#define DLog(source,error)
-#define DLogTrace(msg)
-#define DLogInfo(msg)
-#define DLogWarn(msg)
-#define DLogError(msg)
-#define DLogSeparator()
+#define DLog(source,error)  ((void)0)
+#define DLogTrace(msg)      ((void)0)
+#define DLogInfo(msg)       ((void)0)
+#define DLogWarn(msg)       ((void)0)
+#define DLogError(msg)      ((void)0)
+#define DLogSeparator()     ((void)0)
 
 #endif
 
 #define Log(source,error) StraitX::Output::Printf("[%]: %: %\n",error==Error::Success? "Info " : "Error",source, ErrorNames[(int)error])
-#define LogTrace(msg) StraitX::Output::Printf("[Trace]: %\n", msg)
-#define LogInfo(msg)  StraitX::Output::Printf("[Info ]: %\n", msg)
-#define LogWarn(msg)  StraitX::Output::Printf("[Warn ]: %\n", msg)
-#define LogError(msg) StraitX::Output::Printf("[Error]: %\n", msg)
+#define LogTrace(...) {StraitX::Output::Printf("[Trace]: "); StraitX::Output::Printf(__VA_ARGS__); StraitX::Output::Printf("\n");}
+#define LogInfo(...)  {StraitX::Output::Printf("[Info ]: "); StraitX::Output::Printf(__VA_ARGS__); StraitX::Output::Printf("\n");}
+#define LogWarn(...)  {StraitX::Output::Printf("[Warn ]: "); StraitX::Output::Printf(__VA_ARGS__); StraitX::Output::Printf("\n");}
+#define LogError(...) {StraitX::Output::Printf("[Error]: "); StraitX::Output::Printf(__VA_ARGS__); StraitX::Output::Printf("\n");}
 #define LogSeparator() StraitX::Output::Print("===============================================================")
 
 #endif // STRAITX_LOG_HPP
