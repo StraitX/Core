89a4ef77727e4acf66836990abb05d88488c045c
E1Hephaestus <gefestroyale@gmail.com>
2021-01-11 11:35:02 +0200

    [0.01.16]: Core: Move semantics base

---
+++ Engine/sources/core/move.hpp
@@ -0,0 +1,29 @@
+#ifndef STRAITX_MOVE_HPP
+#define STRAITX_MOVE_HPP
+
+namespace StraitX{
+
+template <typename T>
+struct RemoveReference{
+    typedef T Type;
+};
+
+template <typename T>
+struct RemoveReference<T&>{
+    typedef T Type;
+};
+
+template <typename T>
+struct RemoveReference<T&&>{
+    typedef T Type;
+};
+
+template <typename T>
+constexpr typename RemoveReference<T>::Type&& Move(const T& t){
+    return static_cast<typename RemoveReference<T>::Type&&>(t);
+}
+
+
+}// namespace StraitX::
+
+#endif // STRAITX_MOVE_HPP
