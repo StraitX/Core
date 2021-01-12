87425653f5275269afafa62cb03982f0b0782185
E1Hephaestus <gefestroyale@gmail.com>
2021-01-12 10:05:58 +0200

    [0.01.16]: Vulkan standalone

--- Engine/sources/platform/vulkan.hpp
+++ Engine/sources/platform/vulkan.hpp
@@ -3,9 +3,7 @@
 
 #include "platform/platform_detection.hpp"
 
-#ifdef SX_PLATFORM_LINUX
-    #include <vulkan/vulkan.h>
-#elif defined(SX_PLATFORM_WINDOWS)
+#if defined(SX_PLATFORM_LINUX) || defined(SX_PLATFORM_WINDOWS)
     #include <vulkan/vulkan.h>
 #else
     #error "Your platform does not support vulkan yet."
