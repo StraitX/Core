56c9429e0c9d58657996d831378f5d0713f4c128
E1Hephaestus <gefestroyale@gmail.com>
2021-01-17 11:23:51 +0200

    [0.01.19]: GL: extract possible gpu info

--- Engine/sources/graphics/opengl/graphics_api_impl.cpp
+++ Engine/sources/graphics/opengl/graphics_api_impl.cpp
@@ -2,6 +2,7 @@
 #include "platform/glloader.hpp"
 #include "platform/opengl.hpp"
 #include "core/log.hpp"
+#include "core/string.hpp"
 #include "graphics/opengl/graphics_api_impl.hpp"
 
 namespace StraitX{
@@ -25,6 +26,7 @@ Result GraphicsAPIImpl::Initialize(){
     LogInfo("OpenGL: Loaded Version %",ver);
     LogInfo("OpenGL: Renderer: %", (const char*)glGetString(GL_RENDERER));
     LogInfo("OpenGL: Vendor: %", (const char*)glGetString(GL_VENDOR));
+    LogInfo("OpenGL: Version: %", (const char*)glGetString(GL_VERSION));
 
 
     return Result::Success;
@@ -41,10 +43,30 @@ u32 GraphicsAPIImpl::GetPhysicalGPUCount(){
     return 1;
 }
 
+
+GPUVendor ExtractGPUVendor(const char *string){
+    auto lenght = String::Lenght(string);
+    char *str = (char*)alloca(sizeof(char)*lenght);
+    Memory::Copy(string, str, lenght);
+
+    String::ToLowerCase(str);
+
+    if(String::Contain(str, "nvidia"))
+        return GPUVendor::Nvidia;
+    if(String::Contain(str, "intel"))
+        return GPUVendor::Intel;
+    if(String::Contain(str, "amd"))
+        return GPUVendor::AMD;
+    if(String::Contain(str, "microsoft"))
+        return GPUVendor::Microsoft;
+
+    return GPUVendor::Unknown;
+}
+
 Result GraphicsAPIImpl::GetPhysicalGPUs(PhysicalGPU *array){
     const_cast<u64&>(array->Handle.U64) = 0;
     const_cast<GPUType&>(array->Type) = GPUType::Unknown;
-    const_cast<GPUVendor&>(array->Vendor) = GPUVendor::Unknown;
+    const_cast<GPUVendor&>(array->Vendor) = ExtractGPUVendor((const char *)glGetString(GL_VENDOR));
     const_cast<u32&>(array->QueueFamiliesCount) = 1;
 
     return Result::Success;
