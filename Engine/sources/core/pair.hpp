86992e1abfb7789e8c8a9fdddd0649cc8fa30b8c
E1Hephaestus <gefestroyale@gmail.com>
2021-01-18 13:12:31 +0200

    [0.01.20]: Pair: implemented for no reason

---
+++ Engine/sources/core/pair.hpp
@@ -0,0 +1,57 @@
+#ifndef STRAITX_PAIR_HPP
+#define STRAITX_PAIR_HPP
+
+#include "core/move.hpp"
+
+namespace StraitX{
+
+template<typename F, typename S>
+struct Pair{
+    F First  = {};
+    S Second = {};
+
+    constexpr Pair();
+
+    constexpr Pair(const F &f, const S &s);
+
+    constexpr Pair(F &&f, S &&s);
+
+    constexpr Pair &operator=(const Pair<F, S> &other);
+
+    constexpr Pair &operator=(Pair<F, S> &&other);
+};
+
+template<typename F, typename S>
+constexpr Pair<F,S>::Pair():
+    First(),
+    Second()
+{}
+
+template<typename F, typename S>
+constexpr Pair<F,S>::Pair(const F &f, const S &s):
+    First(f),
+    Second(s)
+{}
+
+template<typename F, typename S>
+constexpr Pair<F,S>::Pair(F &&f, S &&s):
+    First(Move(f)),
+    Second(Move(s))
+{}
+
+template<typename F, typename S>
+constexpr Pair<F, S> &Pair<F,S>::operator=(const Pair<F, S> &other){
+    First = other.First;
+    Second = other.Second;
+}
+
+template<typename F, typename S>
+constexpr Pair<F, S> &Pair<F,S>::operator=(Pair<F, S> &&other){
+    First = Move(other.First);
+    Second = Move(other.Second);
+}
+
+
+}//namespace StraitX::
+
+#endif // STRAITX_PAIR_HPP
