31f3114d71d3cb8b6acd588192b451cae0ed67f6
E1Hephaestus <gefestroyale@gmail.com>
2021-01-23 12:49:06 +0200

    [0.01.20]: Pair: Define copy constructor

--- Engine/sources/core/pair.hpp
+++ Engine/sources/core/pair.hpp
@@ -16,6 +16,10 @@ struct Pair{
 
     constexpr Pair(F &&f, S &&s);
 
+    constexpr Pair(const Pair<F, S> &other);
+
+    constexpr Pair(Pair<F, S> &&other);
+
     constexpr Pair &operator=(const Pair<F, S> &other);
 
     constexpr Pair &operator=(Pair<F, S> &&other);
@@ -40,15 +44,29 @@ constexpr Pair<F,S>::Pair(F &&f, S &&s):
 {}
 
 template<typename F, typename S>
-constexpr Pair<F, S> &Pair<F,S>::operator=(const Pair<F, S> &other){
-    First = other.First;
-    Second = other.Second;
-}
+constexpr Pair<F,S>::Pair(const Pair<F, S> &other):
+    First(other.First),
+    Second(other.Second)
+{}
+
+template<typename F, typename S>
+constexpr Pair<F,S>::Pair(Pair<F, S> &&other):
+    First(Move(other.First)),
+    Second(Move(other.Second))
+{}
 
 template<typename F, typename S>
 constexpr Pair<F, S> &Pair<F,S>::operator=(Pair<F, S> &&other){
     First = Move(other.First);
     Second = Move(other.Second);
+    return *this;
+}
+
+template<typename F, typename S>
+constexpr Pair<F, S> &Pair<F,S>::operator=(const Pair<F, S> &other){
+    First = other.First;
+    Second = other.Second;
+    return *this;
 }
 
 
