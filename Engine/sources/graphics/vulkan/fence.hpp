c56a21121d0a090c3527d1111fe43c9e185a3a97
E1Hephaestus <gefestroyale@gmail.com>
2021-02-07 20:14:22 +0200

    [0.01.22]: Vk: Fence: implemented

---
+++ Engine/sources/graphics/vulkan/fence.hpp
@@ -0,0 +1,38 @@
+#ifndef STRAITX_VULKAN_FENCE_HPP
+#define STRAITX_VULKAN_FENCE_HPP
+
+#include "platform/vulkan.hpp"
+#include "core/assert.hpp"
+#include "core/noncopyable.hpp"
+
+namespace StraitX{
+namespace Vk{
+
+struct Fence: NonCopyable{
+    VkFence Handle = VK_NULL_HANDLE;
+    VkDevice Owner = VK_NULL_HANDLE;
+    
+    sx_inline Fence(VkDevice dev):
+        Owner(dev)
+    {
+        VkFenceCreateInfo info;
+        info.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
+        info.pNext = nullptr;
+        info.flags = 0;
+        CoreFunctionAssert(vkCreateFence(Owner, &info, nullptr, &Handle), VK_SUCCESS, "Vk: Fence: Creation Failed");
+    }
+
+    ~Fence(){
+        vkDestroyFence(Owner, Handle, nullptr);
+    }
+
+    sx_inline void WaitFor(){
+        vkWaitForFences(Owner, 1, &Handle, true, ~0ull);
+        vkResetFences(Owner, 1, &Handle);
+    }
+};
+
+}//namespace Vk::
+}//namespace StraitX::
+
+#endif//STRAITX_VULKAN_FENCE_HPP
