9b5c1b2a31d2cc77565e093e82b6918d5f3ae6f0
E1Hephaestus <39655374+E1Hephaestus@users.noreply.github.com>
2021-02-23 13:45:24 +0200

    [0.01.28]: Windows: VulkanSurface: Implemented

---
+++ Engine/sources/platform/windows/vulkan_surface_impl.cpp
@@ -0,0 +1,23 @@
+#define VK_USE_PLATFORM_WIN32_KHR
+#include "platform/vulkan.hpp"
+#include "platform/vulkan_surface.hpp"
+
+namespace StraitX {
+
+Result VulkanSurface::Create(VkInstance owner, const Window& window) {
+    Owner = owner;
+    VkWin32SurfaceCreateInfoKHR info;
+    info.sType = VK_STRUCTURE_TYPE_XLIB_SURFACE_CREATE_INFO_KHR;
+    info.pNext = nullptr;
+    info.flags = 0;
+    info.hinstance = (HINSTANCE)GetModuleHandle(nullptr);
+    info.hwnd = window.Impl().Handle();
+
+    return ResultError(vkCreateWin32SurfaceKHR(Owner, &info, nullptr, &Handle) != VK_SUCCESS);
+}
+
+void VulkanSurface::Destroy() {
+    vkDestroySurfaceKHR(Owner, Handle, nullptr);
+}
+
+}//namespace StraitX::
