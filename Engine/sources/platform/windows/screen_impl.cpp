5a3470cc7f799ef131a60874a43bd67b8885582c
E1Hephaestus <39655374+E1Hephaestus@users.noreply.github.com>
2021-02-23 11:21:00 +0200

    [0.01.28]: Windows: ScreenImpl: Remove old ctor

--- Engine/sources/platform/windows/screen_impl.cpp
+++ Engine/sources/platform/windows/screen_impl.cpp
@@ -3,10 +3,6 @@
 namespace StraitX {
 namespace Windows {
 
-ScreenImpl::ScreenImpl(void* handle, bool primary, const Size2i& size, const Size2f& dpi):
-	m_Handle(handle), m_IsPrimary(primary), m_Size(size), m_DPI(dpi)
-{}
-
 const Size2i& ScreenImpl::Size()const {
 	return m_Size;
 }
