74c1fb4a05f8716bcadf1500cfed559d2fed727f
E1Hephaestus <gefestroyale@gmail.com>
2021-03-13 14:28:09 +0200

    [0.01.28]:MacOS: Mouse position functions: implemented

---
+++ Engine/sources/platform/macos/mouse_impl.mm
@@ -0,0 +1,41 @@
+#include <CoreGraphics/CoreGraphics.h>
+#include <Cocoa/Cocoa.h>
+#include "platform/mouse.hpp"
+#include "platform/window.hpp"
+#include "platform/macos/sx_application.hpp"
+#include "platform/macos/window_impl.hpp"
+#include "platform/macos/sx_window.hpp"
+
+namespace StraitX{
+
+bool Mouse::IsButtonPressed(Mouse::Button button){
+    MacOS::SXApplication::ProcessEvents();
+    return MacOS::SXApplication::s_MouseState[button];
+}
+
+Point Mouse::GlobalPosition(){
+    CGEventRef event = CGEventCreate(NULL);
+    CGPoint cursor = CGEventGetLocation(event);
+    CFRelease(event);
+    return {(s32)cursor.x, (s32)cursor.y};
+}
+
+Point Mouse::RelativePosition(const Window &window){
+    auto global_pos = GlobalPosition();
+    SXWindow *win_impl = static_cast<MacOS::SXWindowWrapper*>(window.Impl().Handle)->Handle;
+    Point win_pos = {(s32)[win_impl frame].origin.x, (s32)[NSScreen mainScreen].frame.size.height - (s32)[win_impl frame].origin.y - (s32)win_impl.contentView.frame.size.height};
+    return {global_pos.x - win_pos.x, global_pos.y - win_pos.y};
+}
+
+void Mouse::SetGlobalPosition(const Point &position){
+    CGWarpMouseCursorPosition(CGPointMake(position.x, position.y));
+}
+
+void Mouse::SetVisible(bool is_visible){
+    if(is_visible)
+        [NSCursor unhide];
+    else
+        [NSCursor hide];
+}
+
+}//namespace StraitX::
