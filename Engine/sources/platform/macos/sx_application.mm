51ec559941c273940492ea86b98d7c3f70d6ee86
E1Hephaestus <gefestroyale@gmail.com>
2021-03-14 11:44:39 +0200

    [0.01.28]: MacOS: restructure event handling

--- Engine/sources/platform/macos/sx_application.mm
+++ Engine/sources/platform/macos/sx_application.mm
@@ -16,27 +16,38 @@ void SXApplication::ProcessEvents(){
                                             untilDate:nil
                                                inMode:NSDefaultRunLoopMode
                                         dequeue:YES]) != nil){
-        if([e type] != NSEventTypePressure)
-            [NSApp sendEvent:e];
-
-        if([e type] == NSEventTypeKeyDown)
+        switch ([e type]){
+        case NSEventTypeKeyDown:
             s_KeyboardState[ToStraitXKeyCode([e keyCode])] = true;
-        else if([e type] == NSEventTypeKeyUp)
+            break;
+        case NSEventTypeKeyUp:
             s_KeyboardState[ToStraitXKeyCode([e keyCode])] = false;
-        else if([e type] == NSEventTypeFlagsChanged)
+            break;
+        case NSEventTypeFlagsChanged:
             s_KeyboardState[ToStraitXKeyCode([e keyCode])] = ([e modifierFlags] == 256) ? false : true;
-        else if([e type] == NSEventTypeLeftMouseDown)
+            break;
+        case NSEventTypeLeftMouseDown:
             s_MouseState[Mouse::Left] = true;
-        else if([e type] == NSEventTypeRightMouseDown)
+            break;
+        case NSEventTypeRightMouseDown:
             s_MouseState[Mouse::Right] = true;
-        else if([e type] == NSEventTypeOtherMouseDown)
+            break;
+        case NSEventTypeOtherMouseDown:
             s_MouseState[Mouse::Middle] = true;
-        else if([e type] == NSEventTypeLeftMouseUp)
+            break;
+        case NSEventTypeLeftMouseUp:
             s_MouseState[Mouse::Left] = false;
-        else if([e type] == NSEventTypeRightMouseUp)
+            break;
+        case NSEventTypeRightMouseUp:
             s_MouseState[Mouse::Right] = false;
-        else if([e type] == NSEventTypeOtherMouseUp)
+            break;
+        case NSEventTypeOtherMouseUp:
             s_MouseState[Mouse::Middle] = false;
+            break;
+        case NSEventTypePressure:
+            continue;
+        }
+        [NSApp sendEvent:e];
     }
 
 }
