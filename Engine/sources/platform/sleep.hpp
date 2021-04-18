25afd4878fb8bb9e58a48ecb3c0f85c731bd6b94
E1Hephaestus <gefestroyale@gmail.com>
2021-04-18 14:17:53 +0300

    [0.01.29]: Sleep

---
+++ Engine/sources/platform/sleep.hpp
@@ -0,0 +1,12 @@
+#ifndef STRAITX_SLEEP_HPP
+#define STRAITX_SLEEP_HPP
+
+#include "platform/time.hpp"
+
+namespace StraitX{
+
+void Sleep(Time time);
+
+}//namespace StraitX::
+
+#endif//STRAITX_SLEEP_HPP
