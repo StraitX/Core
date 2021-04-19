4d0fa8b58220e05e36db803cb26384261d10a3fb
E1Hephaestus <gefestroyale@gmail.com>
2021-04-19 15:55:24 +0300

    [0.01.28]: MacOS: Clock: Implemented

---
+++ Engine/sources/platform/macos/clock_impl.cpp
@@ -0,0 +1,22 @@
+#include <mach/clock.h>
+#include <mach/mach_time.h>
+#include "platform/clock.hpp"
+
+namespace StraitX{
+namespace MacOS {
+
+static mach_timebase_info_data_t GetTimebaseInfoData(){
+    mach_timebase_info_data_t info = {0, 0};
+    mach_timebase_info(&info);
+    return info;
+}
+
+}//namespace MacOS::
+
+Time Clock::GetMonotonicTime(){
+    static auto freq = MacOS::GetTimebaseInfoData();
+
+    return Microseconds(mach_absolute_time() * freq.numer / freq.denom / 1000);
+}
+
+}//namespace StraitX::
