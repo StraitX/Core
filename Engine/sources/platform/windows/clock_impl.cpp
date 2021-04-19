7cc192fb5a592334aef35333df2ed450ae598b51
E1Hephaestus <39655374+E1Hephaestus@users.noreply.github.com>
2021-04-19 10:13:05 +0200

    [0.01.29]: Windows: Clock impl

---
+++ Engine/sources/platform/windows/clock_impl.cpp
@@ -0,0 +1,24 @@
+#include <Windows.h>
+#include "platform/clock.hpp"
+
+namespace StraitX{
+namespace Windows {
+
+LARGE_INTEGER GetFrequency() {
+    LARGE_INTEGER frequency = {};
+    QueryPerformanceFrequency(&frequency);
+    return frequency;
+}
+
+}//namespace Windows::
+
+Time Clock::GetMonotonicTime(){
+    static double sec_by_freq = 1000000.0 / Windows::GetFrequency().QuadPart;
+
+    LARGE_INTEGER counter = {};
+    QueryPerformanceCounter(&counter);
+
+    return Microseconds(counter.QuadPart * sec_by_freq);
+}
+
+}//namespace StraitX::
