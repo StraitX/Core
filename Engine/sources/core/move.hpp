5b72daca5c0b697084298020b3c7e544be7ff985
E1Hephaestus <gefestroyale@gmail.com>
2021-04-22 14:28:47 +0300

    [0.01.29]: Forward implemented

--- Engine/sources/core/move.hpp
+++ Engine/sources/core/move.hpp
@@ -10,6 +10,16 @@ constexpr typename RemoveReference<T>::Type&& Move(T&& t){
     return static_cast<typename RemoveReference<T>::Type&&>(t);
 }
 
+template <typename T>
+constexpr T&& Forward(typename RemoveReference<T>::Type&& elem){
+    return static_cast<T&&>(elem);
+}
+
+template <typename T>
+constexpr T&& Forward(typename RemoveReference<T>::Type& elem){
+    return static_cast<T&&>(elem);
+}
+
 }// namespace StraitX::
 
 #endif // STRAITX_MOVE_HPP
