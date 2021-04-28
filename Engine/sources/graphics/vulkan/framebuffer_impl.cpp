ea14095f8c86e653c21b582c1ea30aff4c0e38a6
E1Hephaestus <gefestroyale@gmail.com>
2021-04-28 16:36:50 +0300

    [0.01.29]: Vk: refactor Framebuffer

--- Engine/sources/graphics/vulkan/framebuffer_impl.cpp
+++ Engine/sources/graphics/vulkan/framebuffer_impl.cpp
@@ -2,21 +2,21 @@
 #include "platform/memory.hpp"
 #include "platform/vulkan.hpp"
 #include "core/algorithm.hpp"
+#include "graphics/vulkan/gpu.hpp"
 #include "graphics/vulkan/render_pass_impl.hpp"
 #include "graphics/vulkan/framebuffer_impl.hpp"
 
 namespace StraitX{
 namespace Vk{
 
-FramebufferImpl::FramebufferImpl(LogicalGPU &owner, const RenderPass *const pass, const FramebufferProperties &props):
-    Owner(static_cast<Vk::LogicalGPUImpl*>(&owner)),
-    FramebufferSize(props.Size)
+FramebufferImpl::FramebufferImpl(const RenderPass *const pass, const FramebufferProperties &props):
+    m_FramebufferSize(props.Size)
 {
     auto *attachments = (VkImageView *)alloca(props.Attachments.Size() * sizeof(VkImageView));
 
     for(size_t i = 0; i < props.Attachments.Size(); ++i){
         //don't lose the opportunity to store attachment pointers in a single loop
-        Attachments.Push(props.Attachments[i]);
+        m_Attachments.Push(props.Attachments[i]);
 
         attachments[i] = reinterpret_cast<VkImageView>(props.Attachments[i]->ViewHandle().U64);
     }
@@ -25,26 +25,26 @@ FramebufferImpl::FramebufferImpl(LogicalGPU &owner, const RenderPass *const pass
     info.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
     info.pNext = nullptr;
     info.flags = 0;
-    info.renderPass = static_cast<const Vk::RenderPassImpl* const>(pass)->Handle;
+    info.renderPass = static_cast<const Vk::RenderPassImpl* const>(pass)->Handle();
     info.attachmentCount = props.Attachments.Size();
     info.pAttachments = attachments; 
     info.width = Size().x;
     info.height = Size().y;
     info.layers = 1;
 
-    CoreFunctionAssert(vkCreateFramebuffer(Owner->Handle, &info, nullptr, &Handle), VK_SUCCESS, "Vk: FramebufferImpl: Can't create Framebuffer");
+    CoreFunctionAssert(vkCreateFramebuffer(GPU::Get().Handle(), &info, nullptr, &m_Handle), VK_SUCCESS, "Vk: FramebufferImpl: Can't create Framebuffer");
 }
 
 FramebufferImpl::~FramebufferImpl(){
-    vkDestroyFramebuffer(Owner->Handle, Handle, nullptr);
+    vkDestroyFramebuffer(GPU::Get().Handle(), m_Handle, nullptr);
 }
 
 Vector2u FramebufferImpl::Size()const{
-    return FramebufferSize;
+    return m_FramebufferSize;
 }
 
-Framebuffer *FramebufferImpl::NewImpl(LogicalGPU &owner, const RenderPass *const pass, const FramebufferProperties &props){
-    return new(Memory::Alloc(sizeof(Vk::FramebufferImpl))) Vk::FramebufferImpl(owner, pass, props);
+Framebuffer *FramebufferImpl::NewImpl(const RenderPass *const pass, const FramebufferProperties &props){
+    return new(Memory::Alloc(sizeof(Vk::FramebufferImpl))) Vk::FramebufferImpl(pass, props);
 }
 
 void FramebufferImpl::DeleteImpl(Framebuffer *framebuffer){
--- Engine/sources/graphics/vulkan/framebuffer_impl.hpp
+++ Engine/sources/graphics/vulkan/framebuffer_impl.hpp
@@ -1,26 +1,26 @@
 #ifndef STRAITX_VULKAN_FRAMEBUFFER_IMPL_HPP
 #define STRAITX_VULKAN_FRAMEBUFFER_IMPL_HPP
 
+#include "platform/vulkan.hpp"
 #include "core/push_array.hpp"
 #include "graphics/api/framebuffer.hpp"
-#include "graphics/vulkan/logical_gpu_impl.hpp"
 
 namespace StraitX{
 namespace Vk{
 
-struct FramebufferImpl: Framebuffer{
-    Vk::LogicalGPUImpl *const Owner = nullptr;
-    VkFramebuffer Handle = VK_NULL_HANDLE;
-    Vector2u FramebufferSize;
-    PushArray<const GPUTexture *, MaxAttachmentsCount> Attachments;
-
-    FramebufferImpl(LogicalGPU &owner, const RenderPass *const pass, const FramebufferProperties &props);
+class FramebufferImpl: public Framebuffer{
+private:
+    VkFramebuffer m_Handle = VK_NULL_HANDLE;
+    Vector2u m_FramebufferSize;
+    PushArray<const GPUTexture *, MaxAttachmentsCount> m_Attachments;
+public:
+    FramebufferImpl(const RenderPass *const pass, const FramebufferProperties &props);
 
     ~FramebufferImpl();
 
     virtual Vector2u Size()const override;
 
-    static Framebuffer *NewImpl(LogicalGPU &owner, const RenderPass *const pass, const FramebufferProperties &props);
+    static Framebuffer *NewImpl(const RenderPass *const pass, const FramebufferProperties &props);
 
     static void DeleteImpl(Framebuffer *framebuffer);
 };
