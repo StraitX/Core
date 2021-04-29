7cf484af12e3735b10a888b4a8cdf2ece10004e2
E1Hephaestus <gefestroyale@gmail.com>
2021-04-29 14:33:53 +0300

    [0.01.29]: Math: Added Lerp

--- Engine/sources/core/math/linear.hpp
+++ Engine/sources/core/math/linear.hpp
@@ -9,18 +9,23 @@ namespace StraitX{
 
 
 template <typename T>
-constexpr T dot(const Vector2<T> &left, const Vector2<T> &right){
+constexpr T Dot(const Vector2<T> &left, const Vector2<T> &right){
     return left.x * right.x + left.y * right.y;
 }
 
 template <typename T>
-constexpr T dot(const Vector3<T> &left, const Vector3<T> &right){
+constexpr T Dot(const Vector3<T> &left, const Vector3<T> &right){
     return left.x * right.x + left.y * right.y + left.z * right.z;
 }
 
 template <typename T>
-constexpr T dot(const Vector4<T> &left, const Vector4<T> &right){
+constexpr T Dot(const Vector4<T> &left, const Vector4<T> &right){
     return left.x * right.x + left.y * right.y + left.z * right.z + left.w * right.w;
+}
+
+template<typename T>
+constexpr T Lerp(const T &f, const T &s, float c){
+    return f + (s - f)*c;
 }
 
 
