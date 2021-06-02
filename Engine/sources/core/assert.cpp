9e52a81554eac167e6b5b385104b427608cadf69
E1Hephaestus <gefestroyale@gmail.com>
2021-06-02 18:07:35 +0300

    [0.02.01]: Assert: use abort insead of exit

--- Engine/sources/core/assert.cpp
+++ Engine/sources/core/assert.cpp
@@ -11,20 +11,20 @@ void _AssertFail(const char *assertion, const char *filename, unsigned int line,
     char buffer[1024];
     StraitX::BufferPrint(buffer,"Assertion % failed\nfile: %\nline: %\nfunction: %\n", assertion, filename, line, function);
     LogError(buffer);
-    exit(EXIT_FAILURE);
+    abort();
 }
 void _AssertFail(const char *assertion, const char *message, const char *filename, unsigned int line, const char * function){
     char buffer[1024];
     StraitX::BufferPrint(buffer,"Assertion % failed\nmessage: %\nfile: %\nline: %\nfunction: %\n",assertion, message, filename, line, function);
     LogError(buffer);
-    exit(EXIT_FAILURE);
+    abort();
 }
 
 void _CoreAssertFail(const char *assertion, const char *message, const char * function){
     char buffer[1024];
     StraitX::BufferPrint(buffer,"CoreAssertion % failed\nFrom function: %\nMessage: %\n",assertion,function,message);
     LogError(buffer);
-    exit(EXIT_FAILURE);
+    abort();
 }
 
 }//namespace StraitX:
