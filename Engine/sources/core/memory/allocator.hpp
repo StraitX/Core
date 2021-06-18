fc7de210126c320abb1773facec0e63903f2c572
E1Hephaestus <gefestroyale@gmail.com>
2021-06-18 21:28:32 +0300

    [0.02.01]: Default allocators

---
+++ Engine/sources/core/memory/allocator.hpp
@@ -0,0 +1,52 @@
+#ifndef DEFAULT_ALLOCATOR_HPP
+#define DEFAULT_ALLOCATOR_HPP
+
+#include "platform/types.hpp"
+#include "platform/compiler.hpp"
+#include "platform/memory.hpp"
+
+namespace StraitX{
+
+struct DefaultGeneralAllocator{
+	void *Alloc(size_t size);
+
+	void Free(void *ptr);
+};
+
+SX_INLINE void *DefaultGeneralAllocator::Alloc(size_t size){
+	return Memory::Alloc(size);
+}
+
+SX_INLINE void DefaultGeneralAllocator::Free(void *ptr){
+	Memory::Free(ptr);
+}
+
+class DefaultPoolAllocator{
+private:
+	size_t m_BlockSize;
+	size_t m_BlockAlignment;
+public:
+	DefaultPoolAllocator(size_t block_size, size_t block_alignment);
+
+	void *Alloc();
+
+	void Free(void *ptr);
+};
+
+SX_INLINE DefaultPoolAllocator::DefaultPoolAllocator(size_t block_size, size_t block_alignment):
+	m_BlockSize(block_size),
+	m_BlockAlignment(block_alignment)
+{}
+
+SX_INLINE void *DefaultPoolAllocator::Alloc(){
+	return Memory::AlignedAlloc(m_BlockSize, m_BlockAlignment);
+}
+
+SX_INLINE void DefaultPoolAllocator::Free(void *ptr){
+	Memory::AlignedFree(ptr);
+}
+
+
+}//namespace StraitX::
+
+#endif //DEFAULT_ALLOCATOR_HPP
