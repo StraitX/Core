20fc7fbf93bae5cb1d0450b762818d452b3ab987
E1Hephaestus <gefestroyale@gmail.com>
2021-06-20 23:57:25 +0300

    [0.02.01]: Assert: update to print

--- Engine/sources/core/assert.cpp
+++ Engine/sources/core/assert.cpp
@@ -1,29 +1,24 @@
 #include "core/assert.hpp"
-#include "platform/io.hpp"
+#include "core/print.hpp"
 #include "platform/memory.hpp"
 #include <cstdlib>
-#include "core/log.hpp"
-
 
 namespace StraitX{
 
 void _AssertFail(const char *assertion, const char *filename, unsigned int line, const char * function){
     char buffer[1024];
-    StraitX::BufferPrint(buffer,"Assertion % failed\nfile: %\nline: %\nfunction: %\n", assertion, filename, line, function);
-    LogError(buffer);
+    StraitX::WriterPrint(STDErrWriter, nullptr, buffer,"Assertion % failed\nfile: %\nline: %\nfunction: %\n", assertion, filename, line, function);
     abort();
 }
 void _AssertFail(const char *assertion, const char *message, const char *filename, unsigned int line, const char * function){
     char buffer[1024];
-    StraitX::BufferPrint(buffer,"Assertion % failed\nmessage: %\nfile: %\nline: %\nfunction: %\n",assertion, message, filename, line, function);
-    LogError(buffer);
+    StraitX::WriterPrint(STDErrWriter, nullptr, "Assertion % failed\nmessage: %\nfile: %\nline: %\nfunction: %\n",assertion, message, filename, line, function);
     abort();
 }
 
 void _CoreAssertFail(const char *assertion, const char *message, const char * function){
     char buffer[1024];
-    StraitX::BufferPrint(buffer,"CoreAssertion % failed\nFrom function: %\nMessage: %\n",assertion,function,message);
-    LogError(buffer);
+    StraitX::WriterPrint(STDErrWriter, nullptr, "CoreAssertion % failed\nFrom function: %\nMessage: %\n",assertion,function,message);
     abort();
 }
 
