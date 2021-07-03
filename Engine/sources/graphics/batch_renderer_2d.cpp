1e1d899333319206c227b90eb593f0a6658470d8
E1Hephaestus <39655374+E1Hephaestus@users.noreply.github.com>
2021-07-03 03:34:52 +0200

    [0.02.06]: BatchRenderer2D: fix compile errors on window

--- Engine/sources/graphics/batch_renderer_2d.cpp
+++ Engine/sources/graphics/batch_renderer_2d.cpp
@@ -178,10 +178,10 @@ void BatchRenderer2D::DrawRect(Vector2i position, Vector2i size, const Color &co
     position.x -= m_CameraPosition.x;
     position.y -= m_CameraPosition.y;
 
-    m_Vertices[m_VerticesCount + 0] = {{position.x,          position.y,          },texture_coords[0],float(index),color};
-    m_Vertices[m_VerticesCount + 1] = {{position.x,          position.y + size.y, },texture_coords[1],float(index),color};
-    m_Vertices[m_VerticesCount + 2] = {{position.x + size.x, position.y + size.y, },texture_coords[2],float(index),color};
-    m_Vertices[m_VerticesCount + 3] = {{position.x + size.x, position.y,          },texture_coords[3],float(index),color};
+    m_Vertices[m_VerticesCount + 0] = {Vector2f(position.x,          position.y          ),texture_coords[0],float(index),color};
+    m_Vertices[m_VerticesCount + 1] = {Vector2f(position.x,          position.y + size.y ),texture_coords[1],float(index),color};
+    m_Vertices[m_VerticesCount + 2] = {Vector2f(position.x + size.x, position.y + size.y ),texture_coords[2],float(index),color};
+    m_Vertices[m_VerticesCount + 3] = {Vector2f(position.x + size.x, position.y          ),texture_coords[3],float(index),color};
 
     m_Indices[m_IndicesCount + 0] = m_VerticesCount + 0;
     m_Indices[m_IndicesCount + 1] = m_VerticesCount + 1;
