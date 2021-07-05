78c85410500053138da35a070cb058b2bc2492fd
E1Hephaestus <gefestroyale@gmail.com>
2021-07-05 16:31:22 +0300

    [0.02.06]: Linux: EntryPoint: Calculate delta time

--- Engine/sources/platform/linux/entry_point.cpp
+++ Engine/sources/platform/linux/entry_point.cpp
@@ -1,5 +1,6 @@
 #include "platform/platform_runtime.hpp"
 #include "platform/linux/window_impl.hpp"
+#include "platform/clock.hpp"
 
 namespace Linux{
 extern WindowImpl s_Window;
@@ -12,12 +13,20 @@ int main(int argc, char **argv){
 	Result init = PlatformRuntime::Initialize();
 
 	if(init){
-		do{
+
+		Clock frame_clock;
+		float dt = 1.f/60.f;
+		for(;;){
+			frame_clock.Restart();
+
 			Event e;
 			while(Linux::s_Window.PollEvent(e))
 				PlatformRuntime::ProcessEvent(e);
-			
-		}while(PlatformRuntime::Tick(0.016));
+
+			if(!PlatformRuntime::Tick(dt))
+				break;			
+			dt = frame_clock.GetElapsedTime().AsSeconds();	
+		}
 	}
 
 	PlatformRuntime::Finalize();
