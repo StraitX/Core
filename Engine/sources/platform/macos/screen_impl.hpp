8daddcccf50f8a3358008186437dfe96504a8d0f
E1Hephaestus <gefestroyale@gmail.com>
2021-07-06 11:04:14 +0300

    [0.02.06]: MacOS: ScreenImpl now is a struct

--- Engine/sources/platform/macos/screen_impl.hpp
+++ Engine/sources/platform/macos/screen_impl.hpp
@@ -2,34 +2,22 @@
 #define STRAITX_MACOS_SCREEN_IMPL_HPP
 
 #include "platform/result.hpp"
+#include "platform/compiler.hpp"
 #include "platform/events.hpp"
 
 namespace MacOS{
 
-class ScreenImpl{
-private:
-    Size2i m_Size;
-    Size2f m_DPI;
-public:
-    constexpr ScreenImpl(Size2i size, Size2f dpi);
+struct ScreenImpl{
+    Size2i Size;
+    Size2f DPI;
 
-    constexpr const Size2i &Size()const;
-
-    constexpr const Size2f &DPI()const;
+    ScreenImpl(Size2i size, Size2f dpi);
 };
 
-constexpr ScreenImpl::ScreenImpl(Size2i size, Size2f dpi):
-    m_Size(size),
-    m_DPI(dpi)
+SX_INLINE ScreenImpl::ScreenImpl(Size2i size, Size2f dpi):
+    Size(size),
+    DPI(dpi)
 {}
-
-constexpr const Size2i &ScreenImpl::Size()const{
-    return m_Size;
-}
-
-constexpr const Size2f &ScreenImpl::DPI()const{
-    return m_DPI;
-}
 
 }//namespace MacOS::
 
