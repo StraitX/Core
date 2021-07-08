6fff4b39f66bef8121f79bf76ef3af672da696dc
E1Hephaestus <gefestroyale@gmail.com>
2021-07-08 10:57:58 +0300

    [0.02.06]: Arch: Support arm32

--- Engine/sources/platform/arch.hpp
+++ Engine/sources/platform/arch.hpp
@@ -9,6 +9,8 @@
         #define SX_ARCH_X86
     #elif defined(__x86_64__)
         #define SX_ARCH_X86_64
+	#elif defined(__arm__)
+		#define SX_ARCH_ARM_32
 	#elif defined(__aarch64__)
 		#define SX_ARCH_ARM_64
     #else
@@ -24,17 +26,19 @@
     #endif
 #endif
 
-#ifdef SX_ARCH_X86
+#if defined(SX_ARCH_X86)
     #define SX_ARCH_NAME "x86"
 #elif defined(SX_ARCH_X86_64)
     #define SX_ARCH_NAME "x86_64"
+#elif defined(SX_ARCH_ARM_32)
+	#define SX_ARCH_NAME "arm32"
 #elif defined(SX_ARCH_ARM_64)
 	#define SX_ARCH_NAME "arm64"
 #else
 	#error "Unknown processor architecture name"
 #endif
 
-#if defined(SX_ARCH_X86)
+#if defined(SX_ARCH_X86) || defined(SX_ARCH_ARM_32)
 	#define SX_ARCH_32_BIT
 #elif defined(SX_ARCH_X86_64) || defined(SX_ARCH_ARM_64)
 	#define SX_ARCH_64_BIT
