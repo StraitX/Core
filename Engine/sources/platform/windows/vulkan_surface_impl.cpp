ae9fdbf4aac141f94ed20e3a0295d4b602a6d489
E1Hephaestus <39655374+E1Hephaestus@users.noreply.github.com>
2021-07-13 14:07:01 +0200

    [0.02.08]: Windows: Adopt vulkan surface creation changes

--- Engine/sources/platform/windows/vulkan_surface_impl.cpp
+++ Engine/sources/platform/windows/vulkan_surface_impl.cpp
@@ -2,14 +2,14 @@
 #include "platform/vulkan.hpp"
 #include "platform/vulkan_surface.hpp"
 
-Result VulkanSurface::Create(VkInstance owner, const PlatformWindow& window) {
+Result VulkanSurface::Create(VkInstance owner, const PlatformWindowImpl& window) {
     Owner = owner;
     VkWin32SurfaceCreateInfoKHR info;
     info.sType = VK_STRUCTURE_TYPE_XLIB_SURFACE_CREATE_INFO_KHR;
     info.pNext = nullptr;
     info.flags = 0;
     info.hinstance = (HINSTANCE)GetModuleHandle(nullptr);
-    info.hwnd = window.Impl().Handle();
+    info.hwnd = window.Handle();
 
     return ResultError(vkCreateWin32SurfaceKHR(Owner, &info, nullptr, &Handle) != VK_SUCCESS);
 }
