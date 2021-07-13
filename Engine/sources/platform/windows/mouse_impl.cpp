10eaf17feee555c5bb0fb0eb7e0e7d8eeb859a27
E1Hephaestus <39655374+E1Hephaestus@users.noreply.github.com>
2021-07-13 14:41:47 +0200

    [0.02.08]: Windows: Adopt mouse changes

--- Engine/sources/platform/windows/mouse_impl.cpp
+++ Engine/sources/platform/windows/mouse_impl.cpp
@@ -1,7 +1,7 @@
 #include "platform/mouse.hpp"
 #include "platform/windows/virtual_keys.hpp"
 #include "platform/windows/window_impl.hpp"
-#include "platform/window.hpp"
+#include "platform/windows/window_impl.hpp"
 
 bool Mouse::IsButtonPressed(Mouse::Button button) {
     return (GetAsyncKeyState(Windows::MouseButtonToVirtualKey(button)) & 0x8000) != 0;
@@ -11,18 +11,18 @@ Point2i Mouse::GlobalPosition() {
     POINT position = { 0 };
     GetCursorPos(&position);
 
-    auto screen_size = PlatformWindow::Screen().Size;
+    auto screen_size = Windows::WindowImpl::s_MainWindow.Screen().Size;
 
     position.y = screen_size.height - position.y;
     return {position.x,position.y};
 }
 
-Point2i Mouse::RelativePosition(const PlatformWindow &window){
+Point2i Mouse::RelativePosition(){
     POINT position = { 0 };
     GetCursorPos(&position);
-    ScreenToClient(window.Impl().Handle(),&position);
+    ScreenToClient(Windows::WindowImpl::s_MainWindow.Handle(),&position);
 
-    position.y = window.Size().height - position.y;
+    position.y = Windows::WindowImpl::s_MainWindow.Size().height - position.y;
     return {position.x, position.y};
 }
 
@@ -30,7 +30,7 @@ void Mouse::SetGlobalPosition(const Point2i &position){
     
     Point2i new_position;
     new_position.x = position.x;
-    new_position.y = PlatformWindow::Screen().Size.height - position.y;
+    new_position.y = Windows::WindowImpl::s_MainWindow.Screen().Size.height - position.y;
 
     SetCursorPos(new_position.x, new_position.y);
 }
