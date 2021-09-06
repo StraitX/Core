afa99da6285b4af2a4f4afb715387a8e08b91f76
E1Hephaestus <gefestroyale@gmail.com>
2021-09-06 22:07:58 +0300

    Buffer creation fix

--- sources/graphics/api/buffer.cpp
+++ sources/graphics/api/buffer.cpp
@@ -1,8 +1,15 @@
 #include "graphics/api/buffer.hpp"
 #include "graphics/api/graphics_api.hpp"
+#include "core/os/vulkan.hpp"
 
+#ifdef SX_VULKAN_SUPPORTED
+    #include "graphics/api/vulkan/buffer_impl.hpp"
+#endif
 
 Buffer *Buffer::Create(size_t size, BufferMemoryType mem_type, BufferUsage usage){
-
+#ifdef SX_VULKAN_SUPPORTED
+    if(GraphicsAPI::Backend() == GraphicsAPIBackend::Vulkan)
+        return new Vk::BufferImpl(size, mem_type, usage);
+#endif
     return nullptr;
 }
