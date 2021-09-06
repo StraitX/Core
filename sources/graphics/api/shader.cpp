83c6e43c5a81917d56b56b54d90b73f7c3a31a03
E1Hephaestus <gefestroyale@gmail.com>
2021-09-06 18:07:13 +0300

    Basic shader structure

---
+++ sources/graphics/api/shader.cpp
@@ -0,0 +1,7 @@
+#include "graphics/api/shader.hpp"
+#include "core/os/file.hpp"
+
+Shader *Create(ShaderLang lang, ShaderStageBits::Value stage, Span<const char> sources){
+
+    return nullptr;
+}
---
+++ sources/graphics/api/shader.hpp
@@ -0,0 +1,41 @@
+#ifndef STRAITX_SHADER_HPP
+#define STRAITX_SHADER_HPP
+
+#include "core/noncopyable.hpp"
+#include "core/types.hpp"
+#include "core/span.hpp"
+
+constexpr size_t MaxUniformBindings = 8;
+
+
+using ShaderStage = u8;
+
+namespace ShaderStageBits{
+    enum Value: ShaderStage{
+        Vertex                  = 0x01,
+        Geometry                = 0x02,
+        TessellationControl     = 0x04,
+        TessellationEvaluation  = 0x08,
+        Fragment                = 0x10,
+        Compute                 = 0x20
+    };
+}//namespace ShaderStageBits::
+
+enum class ShaderLang: u8{
+    Unknown = 0,
+    GLSL,    
+    //SPIRV,
+};
+
+class Shader: public NonCopyable{
+public:
+    virtual ~Shader() = default;
+
+    virtual ShaderLang Lang()const = 0;
+
+    virtual ShaderStageBits::Value Stage()const = 0;
+
+    static Shader *Create(ShaderLang lang, ShaderStageBits::Value stage, Span<const char> sources);
+};
+
+#endif//STRAITX_SHADER_HPP
