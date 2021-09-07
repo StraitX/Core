a6d99108591db6e7f1caab605ad4a3cf4439fff8
E1Hephaestus <gefestroyale@gmail.com>
2021-09-07 15:31:04 +0300

    Vk: fix sampler creation

--- sources/graphics/api/sampler.cpp
+++ sources/graphics/api/sampler.cpp
@@ -1,7 +1,16 @@
 #include "graphics/api/sampler.hpp"
 #include "graphics/api/graphics_api.hpp"
+#include "core/os/vulkan.hpp"
+
+#ifdef SX_VULKAN_SUPPORTED
+    #include "graphics/api/vulkan/sampler_impl.hpp"
+#endif
 
 Sampler *Sampler::Create(const SamplerProperties &props){
+#ifdef SX_VULKAN_SUPPORTED
+    if(GraphicsAPI::Backend() == GraphicsAPIBackend::Vulkan)
+        return new Vk::SamplerImpl(props);
+#endif
 
     return nullptr;
 }
