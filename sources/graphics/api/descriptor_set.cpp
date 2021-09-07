115c092887d8d529c45d7b4fe75f4f680226a98d
E1Hephaestus <gefestroyale@gmail.com>
2021-09-07 16:06:17 +0300

    DescriptorSet: basic structure

---
+++ sources/graphics/api/descriptor_set.cpp
@@ -0,0 +1,12 @@
+#include "graphics/api/descriptor_set.hpp"
+
+
+DescriptorSetLayout *DescriptorSetLayout::Create(ConstSpan<ShaderBinding> bindings){
+
+    return nullptr;
+}
+
+DescriptorSetPool *DescriptorSetPool::Create(size_t capacity){
+
+    return nullptr;
+}
---
+++ sources/graphics/api/descriptor_set.hpp
@@ -0,0 +1,73 @@
+#ifndef STRAITX_DESCRIPTOR_SET_HPP
+#define STRAITX_DESCRIPTOR_SET_HPP
+
+#include "core/env/compiler.hpp"
+#include "core/push_array.hpp"
+#include "graphics/api/shader.hpp"
+#include "graphics/api/buffer.hpp"
+#include "graphics/api/texture.hpp"
+#include "graphics/api/shader.hpp"
+#include "graphics/api/sampler.hpp"
+
+enum class ShaderBindingType: u8{
+    UniformBuffer = 0,
+    Texture       = 1
+};
+
+struct ShaderBinding{
+    u32 Binding;
+    u32 ElementsCount;
+    ShaderBindingType Type;
+    ShaderStage VisibleStages;
+
+    constexpr ShaderBinding(u32 binding, u32 elements_count, ShaderBindingType type, ShaderStage visible_stages):
+        Binding(binding),
+        ElementsCount(elements_count),
+        Type(type),
+        VisibleStages(visible_stages)
+    {}
+};
+
+// OpenGL supports at least 16 units, and we use last one as a default binding point
+constexpr size_t MaxTexturesBindings = 16 - 1; 
+// this value has nothing to do with the spec
+constexpr size_t MaxUniformBuffersBindings = 16;
+
+constexpr size_t MaxBindings = MaxTexturesBindings + MaxUniformBuffersBindings;
+
+class DescriptorSetLayout: public NonCopyable{
+public:
+	DescriptorSetLayout() = default;
+
+	virtual ~DescriptorSetLayout() = default;
+
+	virtual ConstSpan<ShaderBinding> Bindings()const = 0;
+
+	static DescriptorSetLayout *Create(ConstSpan<ShaderBinding> bindings);
+};
+
+class DescriptorSet: public NonCopyable{
+public:
+	DescriptorSet() = default;
+
+	virtual ~DescriptorSet() = default;
+
+	virtual void UpdateUniformBinding(size_t binding, size_t index, const Buffer *buffer) = 0;
+	
+	virtual void UpdateTextureBinding(size_t binding, size_t index, const Texture2D *texture, const Sampler *sampler) = 0;
+};
+
+class DescriptorSetPool: public NonCopyable{
+public:
+	DescriptorSetPool() = default;
+
+	virtual ~DescriptorSetPool() = default;
+
+	virtual DescriptorSet *Alloc() = 0;
+
+	virtual void Free(DescriptorSet *set) = 0;
+
+	static DescriptorSetPool *Create(size_t capacity);
+};
+
+#endif//STRAITX_DESCRIPTOR_SET_HPP
