96f973ce6e8912e44cabf06ee8b93496e7430a7b
E1Hephaestus <gefestroyale@gmail.com>
2021-12-05 14:47:02 +0200

    FramebufferChain implemented

--- sources/graphics/api/swapchain.cpp
+++ sources/graphics/api/swapchain.cpp
@@ -1,4 +1,5 @@
 #include "core/os/vulkan.hpp"
+#include "core/array.hpp"
 #include "graphics/api/swapchain.hpp"
 #include "graphics/api/graphics_api.hpp"
 
@@ -12,4 +13,42 @@ Swapchain *Swapchain::Create(const Window *window){
         return new Vk::SwapchainImpl(window);
 #endif
     return nullptr;
+}
+
+
+FramebufferChain::FramebufferChain(const Window *window) {
+    m_Swapchain = Swapchain::Create(window);
+
+    Array<AttachmentDescription, 1> s_Attachments = {
+        {
+            TextureLayout::PresentSrcOptimal,
+            TextureLayout::ColorAttachmentOptimal,
+            TextureLayout::PresentSrcOptimal,
+            m_Swapchain->Format(),
+            SamplePoints::Samples_1
+        }
+    };
+
+    m_SwapchainPass = RenderPass::Create({s_Attachments});
+
+    Recreate();
+}
+
+FramebufferChain::~FramebufferChain() {
+    for(auto fb: m_Framebuffers)
+        delete fb;
+    delete m_SwapchainPass;
+    delete m_Swapchain;
+}
+
+void FramebufferChain::Recreate() {
+    m_Framebuffers.Clear();
+    for(Texture2D *image: m_Swapchain->Images()){
+        FramebufferProperties props;
+        props.Pass = m_SwapchainPass;
+        props.Size = m_Swapchain->Size();
+        props.Attachments = {&image, 1};
+
+        m_Framebuffers.Add(Framebuffer::Create(props));
+    }
 }
--- sources/graphics/api/swapchain.hpp
+++ sources/graphics/api/swapchain.hpp
@@ -3,11 +3,12 @@
 
 #include "core/noncopyable.hpp"
 #include "core/span.hpp"
+#include "core/list.hpp"
 #include "graphics/api/semaphore.hpp"
 #include "graphics/api/fence.hpp"
 #include "graphics/api/texture.hpp"
+#include "graphics/api/framebuffer.hpp"
 #include "core/os/window.hpp"
-
 
 class Swapchain: public NonCopyable{
 public:
@@ -38,5 +39,36 @@ public:
 public:
     static Swapchain *Create(const Window *window);
 };
+
+
+class FramebufferChain{
+private:
+    Swapchain *m_Swapchain = nullptr;
+    RenderPass *m_SwapchainPass = nullptr;
+    List<Framebuffer*> m_Framebuffers;
+public:
+    FramebufferChain(const Window *window);
+
+    ~FramebufferChain();
+
+    void Recreate();
+
+    Framebuffer *CurrentFramebuffer()const{
+        return m_Framebuffers[m_Swapchain->Current()];
+    }
+
+    RenderPass *Pass()const{
+        return m_SwapchainPass;
+    }
+
+    void AcquireNext(const Semaphore *signal_semaphore){
+        m_Swapchain->AcquireNext(*signal_semaphore);
+    }
+
+    void PresentCurrent(const Semaphore *wait_semaphore){
+        m_Swapchain->PresentCurrent(*wait_semaphore);
+    }
+};
+
 
 #endif//STRAITX_SWAPCHAIN_HPP
