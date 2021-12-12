f3422e6448d3e68f775e3cba803958b2ee0f38b0
E1Hephaestus <gefestroyale@gmail.com>
2021-12-12 14:05:43 +0200

    Linux: implemented frames skipping

--- sources/platform/linux/stacktrace_impl.cpp
+++ sources/platform/linux/stacktrace_impl.cpp
@@ -13,7 +13,7 @@ void Stacktrace::Capture(void *(&frames)[MaxFramesCount], size_t &captured_frame
 void Printer<Stacktrace>::Print(const Stacktrace &value, void (*writer)(char, void*), void *writer_data){
     char **symbols = backtrace_symbols(value.m_FramePointers, value.m_CapturedFrames);
     char buf[2048];
-    for(int i = 1; i < value.m_CapturedFrames; i++) {
+    for(int i = 1 + value.m_SkipFrames; i < value.m_CapturedFrames; i++) {
         Printer<char>::Print('\n', writer, writer_data);
 
         Dl_info info;
@@ -29,11 +29,11 @@ void Printer<Stacktrace>::Print(const Stacktrace &value, void (*writer)(char, vo
             const char *filename = info.dli_fname;
             void *frame_address = value.m_FramePointers[i];
 
-            snprintf(buf, sizeof(buf), "%-3d 0x%016lx %s :: %s + %zd", i, (size_t)frame_address, filename, proc_name, offset);
+            snprintf(buf, sizeof(buf), "%-3lu 0x%016lx %s :: %s + %zd", i - value.m_SkipFrames, (size_t)frame_address, filename, proc_name, offset);
 
             free(demangled);
         }else{
-            snprintf(buf, sizeof(buf), "%-3d 0x%016lx %s", i, (size_t)value.m_FramePointers[i], symbols[i]);
+            snprintf(buf, sizeof(buf), "%-3lu 0x%016lx %s", i - value.m_SkipFrames, (size_t)value.m_FramePointers[i], symbols[i]);
         }
         Printer<const char*>::Print(buf, writer, writer_data);
         
