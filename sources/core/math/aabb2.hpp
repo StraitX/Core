6c196992d693ed2365f9c4aebe634bb104f7fe45
BloodRedTape <gefestroyale@gmail.com>
2022-03-12 19:23:10 +0200

    AABB implemented

---
+++ sources/core/math/aabb2.hpp
@@ -0,0 +1,40 @@
+#ifndef STRAITX_AABB2_HPP
+#define STRAITX_AABB2_HPP
+
+#include "core/math/vector2.hpp"
+
+template <typename Type>
+struct AABB2 {
+	Vector2<Type> Min;
+	Vector2<Type> Max;
+
+	AABB2(const Vector2<Type> &position, const Vector2<Type> &size):
+		Min(position),
+		Max(position + size)
+	{}
+
+	bool Inside(const Vector2<Type>& point)const{
+		return Min.x <= point.x && point.x <= Max.x
+			&& Min.y <= point.y && point.y <= Max.y;
+	}
+
+	bool Intersects(const AABB2<Type> &other)const{
+		return Min.x <= other.Max.x && Max.x >= other.Min.x
+			&& Min.y <= other.Max.y && Max.y >= other.Min.y;
+	}
+
+	Vector2<Type> Position()const {
+		return Min;
+	}
+
+	Vector2<Type> Size()const {
+		return Max - Min;
+	}
+};
+
+using AABB2s = AABB2<s32>;
+using AABB2u = AABB2<u32>;
+using AABB2f = AABB2<float>;
+using AABB2d = AABB2<double>;
+
+#endif//STRAITX_AABB2_HPP
---
+++ sources/core/math/aabb3.hpp
@@ -0,0 +1,42 @@
+#ifndef STRAITX_AABB3_HPP
+#define STRAITX_AABB3_HPP
+
+#include "core/math/vector3.hpp"
+
+template <typename Type>
+struct AABB3 {
+	Vector3<Type> Min;
+	Vector3<Type> Max;
+
+	AABB3(const Vector3<Type> &position, const Vector3<Type> &size):
+		Min(position),
+		Max(position + size)
+	{}
+
+	bool Inside(const Vector3<Type>& point)const{
+		return Min.x <= point.x && point.x <= Max.x
+			&& Min.y <= point.y && point.y <= Max.y
+			&& Min.z <= point.z && point.z <= Max.z;
+	}
+
+	bool Intersects(const AABB3<Type> &other)const{
+		return Min.x <= other.Max.x && Max.x >= other.Min.x
+			&& Min.y <= other.Max.y && Max.y >= other.Min.y
+			&& Min.z <= other.Max.z && Max.z >= other.Min.z;
+	}
+
+	Vector3<Type> Position()const {
+		return Min;
+	}
+
+	Vector3<Type> Size()const {
+		return Max - Min;
+	}
+};
+
+using AABB3s = AABB3<s32>;
+using AABB3u = AABB3<u32>;
+using AABB3f = AABB3<float>;
+using AABB3d = AABB3<double>;
+
+#endif//STRAITX_AABB3_HPP
