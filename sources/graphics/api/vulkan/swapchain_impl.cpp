60dad93a3412335dee1855dba869a61dbd9c8864
BloodRedTape <gefestroyale@gmail.com>
2022-09-25 19:53:14 +0300

    Now you can store into Swapchain images

--- sources/graphics/api/vulkan/swapchain_impl.cpp
+++ sources/graphics/api/vulkan/swapchain_impl.cpp
@@ -85,7 +85,7 @@ SwapchainImpl::SwapchainImpl(const Window *window):
     info.imageExtent = capabilities.currentExtent;
     info.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE; // XXX For now we support just a single queue
     info.imageArrayLayers = 1;
-    info.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT;
+    info.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_STORAGE_BIT;
 
     SX_VK_ASSERT(vkCreateSwapchainKHR(GPUImpl::Get(), &info, nullptr, &m_Handle), "Vk: Swapchain: Can't create a swapchain");
 
