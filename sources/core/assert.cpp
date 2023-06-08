353328d7fd4a5832779c19e918aa2419bcbd52d1
I.Kostiuk <gefestroyale@gmail.com>
2023-06-08 17:39:21 +0300

    Assert should display message as unicode string

--- sources/core/assert.cpp
+++ sources/core/assert.cpp
@@ -1,21 +1,22 @@
 #include "core/assert.hpp"
 #include "core/print.hpp"
 #include "core/os/memory.hpp"
+#include "core/unicode.hpp"
 #include <cstdlib>
 
 void _AssertFail(const char *assertion, const char *filename, unsigned int line, const char * function){
     char buffer[1024];
-    WriterPrint(*StraitXError, buffer,"Assertion % failed\nfile: %\nline: %\nfunction: %\n", assertion, filename, line, function);
+    WriterPrint(*StraitXError, buffer, SX_UTF8("Assertion % failed\nfile: %\nline: %\nfunction: %\n"), assertion, filename, line, function);
     abort();
 }
 void _AssertFail(const char *assertion, const char *message, const char *filename, unsigned int line, const char * function){
     char buffer[1024];
-    WriterPrint(*StraitXError, "Assertion % failed\nmessage: %\nfile: %\nline: %\nfunction: %\n",assertion, message, filename, line, function);
+    WriterPrint(*StraitXError, SX_UTF8("Assertion % failed\nmessage: %\nfile: %\nline: %\nfunction: %\n"),assertion, message, filename, line, function);
     abort();
 }
 
 void _CoreAssertFail(const char *assertion, const char *message, const char * function){
     char buffer[1024];
-    WriterPrint(*StraitXError, "CoreAssertion % failed\nFrom function: %\nMessage: %\n",assertion,function,message);
+    WriterPrint(*StraitXError, SX_UTF8("CoreAssertion % failed\nFrom function: %\nMessage: %\n"),assertion,function,message);
     abort();
 }
